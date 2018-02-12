cca7b424cddc61beec934e0ac81c6a6fb51ddbf6
davidsluo <davidsluo@users.noreply.github.com>
2018-02-12 06:45:47 -0500

    moved index.html to server; const -> var; port 4000 -> 3000; added pug

--- src/server/index.js
+++ src/server/index.js
@@ -1,14 +1,19 @@
-var express = require('express');
-var app = express();
+const bodyParser = require('body-parser');
+const express = require('express');
 
-var bodyParser = require('body-parser');
+const app = express();
+app.set('view engine', 'pug');
+app.set('views', './views');
 
-app.use(express.static(__dirname +'./../../')); //serves the index.html
+app.use(express.static(__dirname + '/static')); //serves the index.html
+app.use(bodyParser.json());
 
-app.use(bodyParser.json())
+// app.get('/', (req, res) => {
+//     res.send(__dirname)
+// });
 
-app.post('/frogify', (req,res) => {
-    res.send(JSON.stringify({'artist' : req.body.artist}));
+app.post('/frogify', (req, res) => {
+    res.send(JSON.stringify({'artist': req.body.artist}));
 });
 
-app.listen(4000); //listens on port 3000 -> http://localhost:3000/
+app.listen(3000); //listens on port 3000 -> http://localhost:3000/
